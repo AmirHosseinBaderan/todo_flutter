d5a98986fcd4298b9385623d214fb35a63878779
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 21:30:19 +0330

    show task list using task controller

--- lib/widgets/bottom section/item_list.dart
+++ lib/widgets/bottom section/item_list.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_flutter/controllers/task.dart';
 import 'package:todo_flutter/widgets/bottom%20section/item.dart';
 
-Widget itemList() => Container(
-      margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
-      child: ListView.separated(
-          itemBuilder: (context, index) => taskItem(index),
-          separatorBuilder: (context, index) => const Divider(
-                height: 0.5,
-                color: Colors.grey,
-              ),
-          itemCount: 26),
-    );
+Widget itemList() {
+  final taskController = Get.find<TaskController>();
+
+  return Container(
+    margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+    child: Obx(() => ListView.separated(
+        itemBuilder: (context, index) =>
+            taskItem(index, taskController.tasks[index]),
+        separatorBuilder: (context, index) => const Divider(
+              height: 0.5,
+              color: Colors.grey,
+            ),
+        itemCount: taskController.tasks.length)),
+  );
+}
