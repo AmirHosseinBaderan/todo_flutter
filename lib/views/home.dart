2ee28aac898d62f6c9731995e62bcb39ce713e9f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 21:29:24 +0330

    Update home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_flutter/app.dart';
 import 'package:todo_flutter/controllers/navigation.dart';
+import 'package:todo_flutter/controllers/task.dart';
+import 'package:todo_flutter/models/task.dart';
 import 'package:todo_flutter/widgets/home/bottom_section.dart';
 import 'package:todo_flutter/widgets/home/top_section.dart';
 
 class Home extends StatelessWidget {
   final navController = Get.find<NavigationController>();
+  final taskController = Get.find<TaskController>();
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +17,8 @@ class Home extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          taskController.currentTask.value =
+              TaskModel(isComplete: false, text: null, title: null);
           Get.toNamed('/upsertTask')?.then((value) {
             App.changeStatusbarColor(Colors.lightBlue, Brightness.light);
           });
@@ -23,7 +28,7 @@ class Home extends StatelessWidget {
       body: SafeArea(
         child: Stack(
             alignment: Alignment.bottomCenter,
-            children: const [TopSection(), BottomSection()]),
+            children: [TopSection(), const BottomSection()]),
       ),
     );
   }
