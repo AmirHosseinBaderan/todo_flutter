33403aa12953cce49e664ca356d0958c3c102d09
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 16:42:31 +0330

    Update bottom_section.dart

--- lib/widgets/home/bottom_section.dart
+++ lib/widgets/home/bottom_section.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_flutter/widgets/bottom%20section/item_list.dart';
 
 class BottomSection extends StatelessWidget {
   const BottomSection({super.key});
@@ -8,11 +9,12 @@ class BottomSection extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: Get.width,
-      height: Get.height * 0.6,
+      height: Get.height * 0.7,
       decoration: const BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.only(
               topLeft: Radius.circular(20), topRight: Radius.circular(20))),
+      child: itemList(),
     );
   }
 }
