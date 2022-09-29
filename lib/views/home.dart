0b835aa6403b592b5063f097a792c5b8e2305daa
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 20:43:01 +0330

    Update home.dart

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:todo_flutter/app.dart';
 import 'package:todo_flutter/controllers/navigation.dart';
 import 'package:todo_flutter/widgets/home/bottom_section.dart';
 import 'package:todo_flutter/widgets/home/top_section.dart';
@@ -9,9 +10,14 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    App.changeStatusbarColor(Colors.lightBlue, Brightness.light);
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Get.toNamed('/upsertTask')?.then((value) {
+            App.changeStatusbarColor(Colors.lightBlue, Brightness.light);
+          });
+        },
         child: const Icon(Icons.add),
       ),
       body: SafeArea(
