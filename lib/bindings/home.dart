8df29038f4663baf7d4b58366455d8abec77494f
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 21:29:20 +0330

    add task controller dependency

--- lib/bindings/home.dart
+++ lib/bindings/home.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:todo_flutter/controllers/navigation.dart';
+import 'package:todo_flutter/controllers/task.dart';
 
 class HomeBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => NavigationController());
+    Get.lazyPut(() => TaskController());
   }
 }
