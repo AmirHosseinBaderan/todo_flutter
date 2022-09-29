a9fa9bc53a6059cd6014e292630b02529c9f2e45
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 16:42:19 +0330

    create bottom section task list

---
+++ lib/widgets/bottom section/item_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:todo_flutter/widgets/bottom%20section/item.dart';
+
+Widget itemList() => Container(
+      margin: const EdgeInsets.only(top: 20, left: 20, right: 20),
+      child: ListView.separated(
+          itemBuilder: (context, index) => taskItem(index),
+          separatorBuilder: (context, index) => const Divider(
+                height: 0.5,
+                color: Colors.grey,
+              ),
+          itemCount: 10),
+    );
