71b00a3f9b5f1c0fa927c2a7986ebb9932d86f85
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 16:26:39 +0330

    change home view

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,8 +1,8 @@
-import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_flutter/controllers/navigation.dart';
-import 'package:todo_flutter/widgets/shimmer_loading.dart';
+import 'package:todo_flutter/widgets/home/bottom_section.dart';
+import 'package:todo_flutter/widgets/home/top_section.dart';
 
 class Home extends StatelessWidget {
   final navController = Get.find<NavigationController>();
@@ -10,36 +10,15 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Todo App'),
-        centerTitle: true,
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Icon(Icons.add),
-        tooltip: 'Add New Item',
+        child: const Icon(Icons.add),
       ),
-      body: loadingList(10),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: Obx(() => AnimatedBottomNavigationBar(
-            icons: const [
-              Icons.home,
-              Icons.favorite,
-              Icons.delete,
-              Icons.access_alarm
-            ],
-            activeIndex: navController.navIndex.value,
-            gapLocation: GapLocation.center,
-            notchSmoothness: NotchSmoothness.softEdge,
-            leftCornerRadius: 20,
-            rightCornerRadius: 20,
-            backgroundColor: Colors.blue,
-            activeColor: Colors.white,
-            onTap: (index) {
-              navController.changeNav(index);
-            },
-            //other params
-          )),
+      body: SafeArea(
+        child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: const [TopSection(), BottomSection()]),
+      ),
     );
   }
 }
