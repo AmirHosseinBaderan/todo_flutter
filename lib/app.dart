90f236a1f0ffcad6a884c4f0f21941613a0d6404
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-09-29 12:47:13 +0330

    create get app and add pages

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_flutter/bindings/home.dart';
+import 'package:todo_flutter/views/home.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      getPages: [
+        GetPage(name: '/home', page: () => Home(), binding: HomeBindings())
+      ],
+      initialRoute: '/home',
+    );
   }
 }
