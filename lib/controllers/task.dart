1f1fcb08381c566f2ee80d6c3593278500d454c7
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-10-01 15:07:34 +0330

    create list listener

--- lib/controllers/task.dart
+++ lib/controllers/task.dart
@@ -1,10 +1,29 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:todo_flutter/models/task.dart';
 
 class TaskController extends GetxController {
   var tasks = <TaskModel>[].obs;
   Rx<TaskModel> currentTask =
       TaskModel(isComplete: false, text: null, title: null).obs;
+
+  @override
+  void onInit() {
+    var storage = GetStorage();
+    var readItems = storage.read('tasks');
+    if (readItems != null) {
+      tasks.clear();
+      for (var item in readItems) {
+        tasks.add(TaskModel.fromJson(item));
+      }
+    }
+
+    ever(tasks, (value) {
+      storage.write('tasks', tasks.toJson());
+    });
+
+    super.onInit();
+  }
 
   void insertTask(TaskModel task) => tasks.add(task);
 
